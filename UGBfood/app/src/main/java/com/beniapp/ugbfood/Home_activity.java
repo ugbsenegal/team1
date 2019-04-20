30369e86cddcbedf9ae6932da84f034ac2f1dfd0
AmadouBenito <beni.diop.95@gmail.com>
2019-04-20 20:09:15 +0000

    Modification du MenuBar et de l'accueil

--- UGBfood/app/src/main/java/com/beniapp/ugbfood/Home_activity.java
+++ UGBfood/app/src/main/java/com/beniapp/ugbfood/Home_activity.java
@@ -1,6 +1,9 @@
 package com.beniapp.ugbfood;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,7 +15,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Home_activity extends AppCompatActivity {
+public class Home_activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout mDrawerlayout;
     private ActionBarDrawerToggle mToggle;
@@ -20,11 +23,29 @@ public class Home_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer );
         mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open,R.string.close);
         mDrawerlayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.db_accueil:
+               getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,
+                        new accueil_fragment()).commit();
+                break;
+            case R.id.db_menu:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,
+                        new home_fragment()).commit();
+                break;
+        }
+        mDrawerlayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override

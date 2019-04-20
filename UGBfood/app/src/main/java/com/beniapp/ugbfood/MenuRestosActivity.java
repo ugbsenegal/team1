79ee1549e1b9d7c1625920dcab3031fec6b78791
ElHadjiAlyKane <48717379+ElHadjiAlyKane@users.noreply.github.com>
2019-04-20 17:27:40 +0000

    Ajout de l'interface contenant les boutons d'acces aux restaurants publics et prives et aussi d'une autre interface listant les restaurants prives
    La premiere page contient des boutons Restaurants publics et Resrtaurants publics menant vers les zones dediees.
    La seconde interface liste les restaurants prives khairati, KBD etc: en cliquant sur ces boutons on accede ra aux menus correspondants (autres interfaces).

---
+++ UGBfood/app/src/main/java/com/beniapp/ugbfood/MenuRestosActivity.java
@@ -0,0 +1,40 @@
+package com.example.dell.projet_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuRestosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btnPrive = (Button) findViewById(R.id.btnPrive);
+
+        btnPrive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent("restos_prives");
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+}
